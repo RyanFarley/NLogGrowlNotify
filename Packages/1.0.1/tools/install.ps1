bee4ccb16e60162d203ebac09e758d0a7a1c0bcf
Ryan Farley <ryan@farleyzone.com>
2015-07-30 16:36:50 -0700

    Added package for nuget dist

---
+++ Packages/1.0.1/tools/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+if ($host.Version.Major -eq 1 -and $host.Version.Minor -lt 1) {
+    "NOTICE: This package only works with NuGet 1.1 or above. Please update your NuGet install at http://nuget.codeplex.com"
+} else {
+    $project.ProjectItems.Item("nlog.config").Properties.Item("CopyToOutputDirectory").Value = 2
+}
